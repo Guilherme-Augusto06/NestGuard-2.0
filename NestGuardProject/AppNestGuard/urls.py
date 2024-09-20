8107a435dbf4241e297a21cfab890ffc59c96110
Guilherme-Augusto06 <ga375464@gmail.com>
2024-09-20 19:49:40 -0300

    feat: crição do super-usuario

--- NestGuardProject/AppNestGuard/urls.py
+++ NestGuardProject/AppNestGuard/urls.py
@@ -1,3 +1,1 @@
-from django.urls import path
-from . import views
-
+urlpatterns = []
--- NestGuardProject/NestGuardProject/urls.py
+++ NestGuardProject/NestGuardProject/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('AppNestGuard.urls')),
+    path('', include('AppNestGuard.urls')),         # Inclui as urls do app blog
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
