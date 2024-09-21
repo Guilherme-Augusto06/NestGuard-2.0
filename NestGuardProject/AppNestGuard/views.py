3f4cb04022fd95bfe0d5b8fdf349cbd1e2637795
Guilherme-Augusto06 <ga375464@gmail.com>
2024-09-21 20:40:44 -0300

    feat: criação da rota e da view para a pagina de serviços

--- NestGuardProject/AppNestGuard/views.py
+++ NestGuardProject/AppNestGuard/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request, 'homepage.html')
+
+def serviços(request):
+    return render(request, 'serviços.html')
