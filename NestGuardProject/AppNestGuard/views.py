db381412a6bb9374568a8b6638e1126ce31e32cd
Guilherme-Augusto06 <ga375464@gmail.com>
2024-12-22 14:08:16 -0300

    feat: automação criada para envio de emails

--- NestGuardProject/AppNestGuard/views.py
+++ NestGuardProject/AppNestGuard/views.py
@@ -2,6 +2,8 @@ from .models import *
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import OrcamentoForm
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 def homepage(request):
@@ -17,10 +19,34 @@ def orcamento(request):
     if request.method == 'POST':
         form = OrcamentoForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Orçamento registrado com sucesso!')
+            # Salva os dados no banco de dados
+            orcamento = form.save()
+
+            # Monta o conteúdo do e-mail
+            print('email enviado')
+            assunto = f"Novo orçamento registrado por {orcamento.nome_cliente}"
+            mensagem = (
+                f"Detalhes do orçamento:\n\n"
+                f"Nome do cliente: {orcamento.nome_cliente}\n"
+                f"Telefone: {orcamento.telefone_cliente}\n"
+                f"E-mail: {orcamento.email_cliente}\n"
+                f"Descrição do serviço: {orcamento.descricao_servico}\n"
+                f"Data de entrega: {orcamento.data_entrega}\n"
+                f"Tipo de serviço: {orcamento.tipo_servico}\n"
+            )
+            destinatario = ['ga375464@gmail.com']
+
+            # Envia o e-mail
+            try:
+                send_mail(assunto, mensagem, settings.EMAIL_HOST_USER, destinatario)
+                messages.success(request, 'Orçamento registrado e e-mail enviado com sucesso!')
+            except Exception as e:
+                messages.error(request, f"Erro ao enviar e-mail: {e}")
+                print(f"Erro ao enviar e-mail: {e}")
+
             return redirect('orcamento')  # Redireciona para a mesma página após o sucesso
         else:
+            print('erro no registro')
             messages.error(request, 'Houve um erro no registro. Verifique os dados e tente novamente.')
     else:
         form = OrcamentoForm()
--- NestGuardProject/NestGuardProject/settings.py
+++ NestGuardProject/NestGuardProject/settings.py
@@ -130,3 +130,10 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'nestguardemails@gmail.com'  
+EMAIL_HOST_PASSWORD = 'rskm lthg ffbd tihs'  
